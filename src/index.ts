f394c522c861e914a9037a5d3402949c89f94ab0
Robert Kania <cezaryrk@yahoo.de>
2018-02-09 17:26:04 +0100

    forgot to add to index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./libs/utils/ClassLoader"
 export * from "./libs/utils/PlatformUtils"
+export * from "./libs/utils/FileUtils"
 export * from "./libs/exceptions/NestedException"
 export * from "./libs/exceptions/TodoException"
 export * from "./types"
