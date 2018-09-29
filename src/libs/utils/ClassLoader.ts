2504dac56261136f7d41aeb556fb2cb4ac0df0f0
Robert Kania <cezaryrk@yahoo.de>
2018-09-29 14:54:09 +0200

    add static method for classname and function

--- src/libs/utils/ClassLoader.ts
+++ src/libs/utils/ClassLoader.ts
@@ -121,4 +121,32 @@ export class ClassLoader {
     }
     return _path;
   }
+
+  static getClassName(klass: string | Function) {
+    if (_.isString(klass)) {
+      return klass;
+    } else if (_.isFunction(klass)) {
+      return klass.name;
+    } else if (_.isObject(klass)) {
+      return klass.constructor.name;
+    } else {
+      throw new Error('class not found!');
+    }
+  }
+
+
+  static getFunction(klass: string | Function) {
+    if (_.isString(klass)) {
+      // TODO create error class
+      throw new Error('class not found! 02');
+    } else if (_.isFunction(klass)) {
+      return klass;
+    } else if (_.isObject(klass)) {
+      return klass.constructor;
+    } else {
+      // TODO create error class
+      throw new Error('class not found! 01');
+    }
+  }
+
 }
