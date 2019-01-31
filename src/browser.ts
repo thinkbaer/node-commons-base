f0eca31f00554788e9099610f2bf01641a1ba195
Robert Kania <cezaryrk@yahoo.de>
2019-01-31 11:46:08 +0100

    refactor: make a browser ts for nodejs free imports

---
+++ src/browser.ts
@@ -0,0 +1,6 @@
+export * from "./libs/Constants"
+export * from "./libs/exceptions/NestedException"
+export * from "./libs/exceptions/TodoException"
+export * from "./libs/exceptions/NotYetImplementedError"
+export * from "./libs/exceptions/NotSupportedError"
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * from "./libs/Constants"
+export * from './browser';
+
+
 export * from "./libs/utils/ClassLoader"
 export * from "./libs/utils/PlatformUtils"
 export * from "./libs/utils/FileUtils"
-export * from "./libs/exceptions/NestedException"
-export * from "./libs/exceptions/TodoException"
 
