fbe7f5a6a34df77d637b7801106d0912ed684f78
Robert Kania <cezaryrk@yahoo.de>
2019-01-31 11:45:44 +0100

    test: adapt tests for async

--- test/functional/01_classloader.spec.ts
+++ test/functional/01_classloader.spec.ts
@@ -7,7 +7,7 @@ import {ClassLoader} from "../../src";
 class classloaderSpec {
 
   @test
-  async 'source in embedded'() {
+  async 'load sync from directory'() {
     let tmp = null;
     if(Reflect && Reflect['getOwnMetadata']){
       tmp = Reflect['getOwnMetadata'];
@@ -19,6 +19,67 @@ class classloaderSpec {
     path = path.replace(__dirname,'.');
     expect(path).to.eq('./classes/Cls01.ts');
 
+    if(tmp){
+      Reflect['getOwnMetadata'] = tmp;
+    }
+  }
+
+
+  @test
+  async 'load async from directory'() {
+    let tmp = null;
+    if(Reflect && Reflect['getOwnMetadata']){
+      tmp = Reflect['getOwnMetadata'];
+      Reflect['getOwnMetadata'] = null;
+    }
+    let clss = await ClassLoader.importClassesFromDirectoriesAsync([__dirname+'/classes/*']);
+    expect(clss).to.have.length(1);
+    let path = clss[0]['__SOURCE__'];
+    path = path.replace(__dirname,'.');
+    expect(path).to.eq('./classes/Cls01.ts');
+
+    if(tmp){
+      Reflect['getOwnMetadata'] = tmp;
+    }
+  }
+
+
+  @test
+  async 'load sync from directories'() {
+    let tmp = null;
+    if(Reflect && Reflect['getOwnMetadata']){
+      tmp = Reflect['getOwnMetadata'];
+      Reflect['getOwnMetadata'] = null;
+    }
+    let clss = ClassLoader.importClassesFromDirectories([
+      __dirname+'/files/classes/*',
+      __dirname+'/files/classes/subclasses/*']);
+    expect(clss).to.have.length(3);
+    let path = clss[0]['__SOURCE__'];
+    expect(clss[0]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/Cls01.ts');
+    expect(clss[1]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/subclasses/Cls02.ts');
+    expect(clss[2]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/subclasses/Cls03.ts');
+    if(tmp){
+      Reflect['getOwnMetadata'] = tmp;
+    }
+  }
+
+
+  @test
+  async 'load async from directories'() {
+    let tmp = null;
+    if(Reflect && Reflect['getOwnMetadata']){
+      tmp = Reflect['getOwnMetadata'];
+      Reflect['getOwnMetadata'] = null;
+    }
+    let clss = await ClassLoader.importClassesFromDirectoriesAsync([
+      __dirname+'/files/classes/*',
+      __dirname+'/files/classes/subclasses/*']);
+    expect(clss).to.have.length(3);
+    let path = clss[0]['__SOURCE__'];
+    expect(clss[0]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/Cls01.ts');
+    expect(clss[1]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/subclasses/Cls02.ts');
+    expect(clss[2]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/subclasses/Cls03.ts');
     if(tmp){
       Reflect['getOwnMetadata'] = tmp;
     }
