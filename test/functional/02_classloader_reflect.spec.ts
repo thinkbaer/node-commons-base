77b001d97632a07b7cde1e3da8fed56dfe84b3c2
Robert Kania <cezaryrk@yahoo.de>
2019-01-31 21:22:07 +0100

    test: add test for import classes from any

--- test/functional/02_classloader_reflect.spec.ts
+++ test/functional/02_classloader_reflect.spec.ts
@@ -1,7 +1,7 @@
 import {suite, test} from "mocha-typescript";
 import {expect} from "chai";
 import "reflect-metadata";
-import {ClassLoader} from "../../src";
+import {__SOURCE__, ClassLoader} from "../../src";
 
 
 @suite('test/classloading/reflect')
@@ -11,17 +11,37 @@ class classloader_reflectSpec {
   async 'source in metadata'() {
     let clss = ClassLoader.importClassesFromDirectories([__dirname+'/classes/*']);
     expect(clss).to.have.length(1);
-    let path = Reflect.getOwnMetadata('__SOURCE__',clss[0]);
+    let path = Reflect.getOwnMetadata(__SOURCE__,clss[0]);
     path = path.replace(__dirname,'.');
     expect(path).to.eq('./classes/Cls01.ts');
   }
+
+  @test
+  async 'source in metadata async'() {
+    let clss = await ClassLoader.importClassesFromDirectoriesAsync([__dirname+'/classes/*']);
+    expect(clss).to.have.length(1);
+    let path = Reflect.getOwnMetadata(__SOURCE__,clss[0]);
+    path = path.replace(__dirname,'.');
+    expect(path).to.eq('./classes/Cls01.ts');
+  }
+
 
 
   @test
   async 'load from any'() {
     let clss = ClassLoader.importClassesFromAny([__dirname+'/classes/Cls01.ts']);
     expect(clss).to.have.length(1);
-    let path = Reflect.getOwnMetadata('__SOURCE__',clss[0]);
+    let path = Reflect.getOwnMetadata(__SOURCE__,clss[0]);
+    path = path.replace(__dirname,'.');
+    expect(path).to.eq('./classes/Cls01.ts');
+
+  }
+
+  @test
+  async 'load from any async'() {
+    let clss = await ClassLoader.importClassesFromAnyAsync([__dirname+'/classes/Cls01.ts']);
+    expect(clss).to.have.length(1);
+    let path = Reflect.getOwnMetadata(__SOURCE__,clss[0]);
     path = path.replace(__dirname,'.');
     expect(path).to.eq('./classes/Cls01.ts');
 
