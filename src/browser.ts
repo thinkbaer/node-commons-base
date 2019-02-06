b8d4b39da180719f1b5e9f93467dc056a4920a4e
Robert Kania <cezaryrk@yahoo.de>
2019-02-06 12:20:34 +0100

    fix: add generic metadata cache

--- src/browser.ts
+++ src/browser.ts
@@ -1,4 +1,7 @@
 export * from "./libs/Constants"
+
+export * from "./libs/MetaArgs";
+
 export * from "./libs/exceptions/NestedException"
 export * from "./libs/exceptions/TodoException"
 export * from "./libs/exceptions/NotYetImplementedError"
--- src/libs/Constants.ts
+++ src/libs/Constants.ts
@@ -2,3 +2,5 @@
 export type StringOrFunction = string | Function
 
 export const __SOURCE__ = '__SOURCE__';
+
+
