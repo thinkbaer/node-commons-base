beb7185b4fb2766de2f2ca0b95e50c6a2ee0f0c7
Robert Kania <cezaryrk@yahoo.de>
2020-08-11 20:18:02 +0200

    test: update

--- test/functional/01_classloader.spec.ts
+++ test/functional/01_classloader.spec.ts
@@ -1,25 +1,25 @@
-import {suite, test} from "mocha-typescript";
-import {expect} from "chai";
-import {ClassLoader} from "../../src";
+import {suite, test} from 'mocha-typescript';
+import {expect} from 'chai';
+import {ClassLoader} from '../../src';
 
 
 @suite('test/classloading')
-class classloaderSpec {
+class ClassloaderSpec {
 
   @test
   async 'load sync from directory'() {
     let tmp = null;
-    if(Reflect && Reflect['getOwnMetadata']){
+    if (Reflect && Reflect['getOwnMetadata']) {
       tmp = Reflect['getOwnMetadata'];
       Reflect['getOwnMetadata'] = null;
     }
-    let clss = ClassLoader.importClassesFromDirectories([__dirname+'/classes/*']);
+    const clss = ClassLoader.importClassesFromDirectories([__dirname + '/classes/*']);
     expect(clss).to.have.length(1);
     let path = clss[0]['__SOURCE__'];
-    path = path.replace(__dirname,'.');
+    path = path.replace(__dirname, '.');
     expect(path).to.eq('./classes/Cls01.ts');
 
-    if(tmp){
+    if (tmp) {
       Reflect['getOwnMetadata'] = tmp;
     }
   }
@@ -28,17 +28,17 @@ class classloaderSpec {
   @test
   async 'load async from directory'() {
     let tmp = null;
-    if(Reflect && Reflect['getOwnMetadata']){
+    if (Reflect && Reflect['getOwnMetadata']) {
       tmp = Reflect['getOwnMetadata'];
       Reflect['getOwnMetadata'] = null;
     }
-    let clss = await ClassLoader.importClassesFromDirectoriesAsync([__dirname+'/classes/*']);
+    const clss = await ClassLoader.importClassesFromDirectoriesAsync([__dirname + '/classes/*']);
     expect(clss).to.have.length(1);
     let path = clss[0]['__SOURCE__'];
-    path = path.replace(__dirname,'.');
+    path = path.replace(__dirname, '.');
     expect(path).to.eq('./classes/Cls01.ts');
 
-    if(tmp){
+    if (tmp) {
       Reflect['getOwnMetadata'] = tmp;
     }
   }
@@ -47,19 +47,19 @@ class classloaderSpec {
   @test
   async 'load sync from directories'() {
     let tmp = null;
-    if(Reflect && Reflect['getOwnMetadata']){
+    if (Reflect && Reflect['getOwnMetadata']) {
       tmp = Reflect['getOwnMetadata'];
       Reflect['getOwnMetadata'] = null;
     }
-    let clss = ClassLoader.importClassesFromDirectories([
-      __dirname+'/files/classes/*',
-      __dirname+'/files/classes/subclasses/*']);
+    const clss = ClassLoader.importClassesFromDirectories([
+      __dirname + '/files/classes/*',
+      __dirname + '/files/classes/subclasses/*']);
     expect(clss).to.have.length(3);
-    let path = clss[0]['__SOURCE__'];
-    expect(clss[0]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/Cls01.ts');
-    expect(clss[1]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/subclasses/Cls02.ts');
-    expect(clss[2]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/subclasses/Cls03.ts');
-    if(tmp){
+    const path = clss[0]['__SOURCE__'];
+    expect(clss[0]['__SOURCE__'].replace(__dirname, '.')).to.eq('./files/classes/Cls01.ts');
+    expect(clss[1]['__SOURCE__'].replace(__dirname, '.')).to.eq('./files/classes/subclasses/Cls02.ts');
+    expect(clss[2]['__SOURCE__'].replace(__dirname, '.')).to.eq('./files/classes/subclasses/Cls03.ts');
+    if (tmp) {
       Reflect['getOwnMetadata'] = tmp;
     }
   }
@@ -68,19 +68,19 @@ class classloaderSpec {
   @test
   async 'load async from directories'() {
     let tmp = null;
-    if(Reflect && Reflect['getOwnMetadata']){
+    if (Reflect && Reflect['getOwnMetadata']) {
       tmp = Reflect['getOwnMetadata'];
       Reflect['getOwnMetadata'] = null;
     }
-    let clss = await ClassLoader.importClassesFromDirectoriesAsync([
-      __dirname+'/files/classes/*',
-      __dirname+'/files/classes/subclasses/*']);
+    const clss = await ClassLoader.importClassesFromDirectoriesAsync([
+      __dirname + '/files/classes/*',
+      __dirname + '/files/classes/subclasses/*']);
     expect(clss).to.have.length(3);
-    let path = clss[0]['__SOURCE__'];
-    expect(clss[0]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/Cls01.ts');
-    expect(clss[1]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/subclasses/Cls02.ts');
-    expect(clss[2]['__SOURCE__'].replace(__dirname,'.')).to.eq('./files/classes/subclasses/Cls03.ts');
-    if(tmp){
+    const path = clss[0]['__SOURCE__'];
+    expect(clss[0]['__SOURCE__'].replace(__dirname, '.')).to.eq('./files/classes/Cls01.ts');
+    expect(clss[1]['__SOURCE__'].replace(__dirname, '.')).to.eq('./files/classes/subclasses/Cls02.ts');
+    expect(clss[2]['__SOURCE__'].replace(__dirname, '.')).to.eq('./files/classes/subclasses/Cls03.ts');
+    if (tmp) {
       Reflect['getOwnMetadata'] = tmp;
     }
   }
